ebd4809fa27e070fa2d985340600f7a235f7e2c0
eGroul <eegroul@gmail.com>
2025-05-14 01:13:16 +0900

    cccc

--- ExelToJson/Assets/DataTable.cs
+++ ExelToJson/Assets/DataTable.cs
@@ -6,5 +6,6 @@ using UnityEngine;
 [ExcelAsset]
 public class DataTable : ScriptableObject
 {
+	//public List<EntityType> Difintion; // Replace 'EntityType' to an actual type that is serializable.
 	public List<EntityType> Data; // Replace 'EntityType' to an actual type that is serializable.
 }
